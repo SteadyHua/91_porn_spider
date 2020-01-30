4101af0a2f730213077b218bf9d9e371dff20b67
eqblog <i@t667.com>
2020-01-30 21:59:54 +0800

    修正header信息错误

--- 91_js.py
+++ 91_js.py
@@ -100,7 +100,7 @@ def main():
             try:
                 header=req.headers
                 header['X-Forwarded-For']=random_ip()
-                header['Accept-Language']="Accept-Language: zh-CN,zh;q=0.9"
+                header['Accept-Language']="zh-CN,zh;q=0.9"
                 base_req = req.get(url=base_url + key,headers=header)
                 #print(base_req.text)
                 ifm=re.findall('strencode\((.*?)\)',base_req.text)
